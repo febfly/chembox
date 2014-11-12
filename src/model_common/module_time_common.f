538cc1211ded54bd4658ca7a5366f54803cd3a7f
febfly <zhangyz.pku@gmail.com>
2014-11-11 20:57:42 -0500

    finish new time_common

--- src/model_common/module_time_common.f
+++ src/model_common/module_time_common.f
@@ -1,124 +1,198 @@
       module module_time_common
-      use module_model_parameter,only:DP,TSCHEM=>TS_CHEM_MIN,
-     +    TSM3D=>TS_MODEL_MIN, TSOUT=>TS_OUTPUT_MIN
+      use module_model_parameter,only: DP,TS_MODEL, TS_CHEM, 
+     +    TS_OUTPUT
       implicit none
+
 !=======================================================================
-! t_total_min   : total simulation time in minute
-! t_elapse_min  : time at the end of current step in minute
-! t_remain_min  : time remaining at the end of current step in minute
-! t_laststep_min: time at the end of last step and at the beginning of 
-!                 the current one, in minute
-! nstep         : total number of time steps
-! istep         : steps finished at the end of current step
-! t_st_min      : astronomical Julian day * 1440 at the beginning of 
+! timestep(NPROCESS)   : timestep of each process in second
+! timestep_n(NPROCESS) : ratio between a timestep and the base timestep
+!                        must be an integer >= 1
+! ifdoit(NPROCESS)     : if the model needs to compute a particular
+!                        process in the following time step
+!-----------------------------------------------------------------------
+!
+! modle_status  : the status of the model iteration
+!   = FLAG_START  : the first loop
+!   = FLAG_NORMAL : a normal loop
+!   = FLAG_END    : itstep has reached ntstep, the simulation has
+!                   finished
+!
+!-----------------------------------------------------------------------
+! t_total_s     : total simulation time in second
+! t_elapse_s    : time at the end of current step in second
+! t_remain_s    : time remaining at the end of current step in second
+! ntstep        : total number of time steps
+! itstep        : count of time steps, from 0-ntstep-1
+!                 when itstep >= ntstep, model_status become FLAG_END
+! t_st_s        : astronomical Julian day * 86500 at the beginning of 
 !                 simulation
-! t_ed_min      :astronomical Julian day * 1440 at the end of simulation
+! t_ed_s        :astronomical Julian day * 86500 at the end of simulation
 ! 
 !=======================================================================
 
-      integer,parameter:: FLAG_END=2, FLAG_LAST=1, FLAG_NORMAL=0
+      !time step control
+      integer, parameter :: NPROCESS = 3
+      integer, parameter :: IP_BASE = 1, IP_CHEM  = 2, IP_OUTPUT = 3
+      real(kind=DP), dimension(NPROCESS) :: timestep
+      integer, dimension(NPROCESS)       :: timestep_n
+      logical, dimension(NPROCESS)       :: ifdoit
+      integer                            :: ntstep, itstep
 
-      real(kind=DP) :: t_total_min
-      real(kind=DP) :: t_remain_min, t_elapse_min
-      integer       :: ntstep, itstep
-      real(kind=DP) :: t_st_min, t_ed_min
-      integer       :: year, jday, hour, minute, month, day
-      real(kind=DP) :: second
-      !integer       :: year_ls, jday_ls, 
+      !model status control
+      integer           :: model_status
+      integer, parameter:: FLAG_START=0, FLAG_END=2, FLAG_NORMAL=1
 
-      real(kind=DP) :: ts_model_min
-      real(kind=DP) :: ts_chem_min
-      real(kind=DP) :: ts_output_min
-      logical       :: if_time_output
+      !time variables
+      real(kind=DP) :: t_total_s
+      real(kind=DP) :: t_remain_s, t_elapse_s
+      real(kind=DP) :: t_st_s, t_ed_s
+      integer       :: year_st, hour_st, minute_st, month_st,
+     +                 day_st, second_st
+      integer       :: year_ed, hour_ed, minute_ed, month_ed,
+     +                 day_ed, second_ed
+      integer       :: year, month, day, hour, minute, second
+      real(kind=DP) :: jday
 
-      real(kind=DP),private ::last_output_step, dstep_output
 
-      public :: time_step_init
-      public :: time_step_next
+      public :: time_init
+      public :: time_update
 
+      private:: time_settimestep
+      private:: time_settime
+      
       contains
 
-!=======================================================================
-! time_step_init
-!     1. Initialize time step
-!     2. Calculate simulation time in minute
-!     3. Initialize other time variables such as year, month, day, 
-!        hour, etc.
-!=======================================================================
-      subroutine time_step_init(y0,m0,d0,h0,mi0,s0,
-     +                          y1,m1,d1,h1,mi1,s1)
-      use julday_mod, only : julday
-      integer, intent(in) :: y0, m0, d0, h0, mi0
-      integer, intent(in) :: y1, m1, d1, h1, mi1
-      real(kind=DP),intent(in) :: s0, s1
-      real(kind=DP) :: frac_day0, frac_day1
+!======================================================================
+! time_init
+!     Initialize the module
+!=====================================================================
+      subroutine time_init
+      real(kind=DP) :: nts_chem_tmp, nts_output_tmp
 
-      ts_model_min = TSM3D
-      ts_chem_min  = TSCHEM
-      ts_output_min= TSOUT
-      dstep_output = ts_output_min/ts_model_min
+      !setup time step for varied process
+      timestep(IP_BASE)   = TS_MODEL
+      timestep(IP_CHEM)   = TS_CHEM
+      timestep(IP_OUTPUT) = TS_OUTPUT
+      call time_settimestep
 
-      frac_day0 = d0+float(h0)/24d0+float(mi0)/1440d0+s0/86400d0
-      frac_day1 = d1+float(h1)/24d0+float(mi1)/1440d0+s1/86400d0
-      t_st_min = julday(y0,m0,frac_day0)*1440d0
-      t_ed_min = julday(y1,m1,frac_day1)*1440d0
-      if (t_st_min.ge.t_ed_min) then
+      !initialize time variables
+      call time_settime
+
+      endsubroutine time_init
+
+!======================================================================      
+! time_update
+!     Update the time variables at the end of an iteration and control 
+!     what process to compute in the next time step
+!======================================================================
+      subroutine time_update
+      use julday_mod,   only: julday
+      use util_time_mod,only: jd2ymdhms
+   
+      integer :: ymd, hms
+      real(kind=DP) :: jd, rem
+      integer :: i
+ 
+      itstep = itstep + 1
+
+      !update time variables for next time step
+      if (itstep.lt.ntstep) then
+         t_remain_s = t_remain_s - timestep(IP_BASE)
+         t_elapse_s = t_elapse_s + timestep(IP_BASE)
+         jd = (t_st_s + t_elapse_s)/86400d0
+         call jd2ymdhms(jd, year, month, day, hour, minute, second)
+         jday = jd - julday(year,1, 1d0)
+         flag = FLAG_NORMAL
+      elseif (itstep.ge.ntstep) then
+         print*, 'Simulation finished'
+         flag = FLAG_END 
+      endif
+ 
+      !determine whether process i will be executed in next time step
+      do i=1,NPROCESS
+         if (mod(itstep,timestep_n(i)).eq.0) then
+            ifdoit(i)=.true.
+         else 
+            ifdoit(i)=.false.
+         endif
+      enddo
+
+      endsubroutine time_update
+
+!======================================================================      
+! time_settimestep
+!     Set up the timestep control when the module is initialized
+!======================================================================
+      subroutine time_settimestep
+      use module_util, only: isint
+      integer       :: i
+      real(kind=DP) :: tmp
+
+      !base time step
+      if (timestep(IP_BASE).le.0) then
+         print*,'Error: base time step must be greater than 0'
+         stop
+      endif
+
+      !calculate the relationship with base time step for each process
+      !timestep_n is t
+      do i=1,NPROCESS
+         if (timestep(i).le.0) timestep(i)=timestep(IP_BASE)
+         tmp = timestep(i)/timestep(IP_BASE)
+         
+         if (.not.isint(tmp)) then
+            print*,'Error: the time step for each process must be a'
+     +        //'multiple of the base time step', 
+     +        timestep(i), timestep(IP_BASE)
+            stop
+         endif
+
+         timestep_n(i)=nint(tmp)
+
+         if (timestep_n(i).lt.1) then
+            print*,'Error: the time step for each process must be '
+     +        // 'equal to or greater than the base time step',
+     +        timestep(i), timestep(IP_BASE)
+            stop
+         endif
+      enddo
+      endsubroutine time_settimestep
+
+!======================================================================      
+! time_settime
+!     Set the time variables when the module is initialized
+!======================================================================
+      subroutine time_settime
+      real(kind=DP) :: frac_day_st, frac_day_ed
+      
+      frac_day_st = day_st+float(hour_st)/24d0+float(minute_st)/1440d0
+     +            + second_st/86400d0
+      frac_day_ed = day_ed+float(hour_ed)/24d0+float(minute_ed)/1440d0
+     +            + second_ed/86400d0
+      t_st_s      = julday(year_st, month_st, frac_day_st)*86400d0
+      t_ed_s      = julday(year_ed, month_ed, frac_day_ed)*86400d0
+
+      if (t_st_s.ge.t_ed_s) then
          print*,'Error: starting time is later than ending time'
          stop
-      else 
-         t_total_min    = t_ed_min - t_st_min
-         t_remain_min   = t_total_min
-         t_elapse_min   = 0d0
-         ntstep  = ceiling(t_total_min/ts_model_min)
-         itstep = 0
-         year   = y0
-         month  = m0
-         day    = d0
-         minute = mi0
-         second = s0
-         jday   = int(t_st_min/1440d0 - julday(y0,1,1d0) + 1)
       endif
-      
-      endsubroutine time_step_init
 
-!======================================================================
-! time_step_next
-!     move to the next time step of ts_model_min
-!     flag = 0, normal
-!          = 1, last step
-!          = 2, simulation finished   
-!======================================================================
-      function time_step_next() result(flag)
-      use julday_mod,only:julday
-      use util_time_mod,only:jd2ymdhms
-      integer :: ymd, hms, flag
-      real(kind=DP) :: jd, rem
-      itstep = itstep + 1
-      if (itstep.lt.ntstep) then 
-         t_remain_min  = t_remain_min - ts_model_min
-         t_elapse_min  = t_elapse_min + ts_model_min
-         jd = (t_st_min+t_elapse_min)/1440d0
-         call jd2ymdhms(jd,year,month,day,hour,minute,second)
-         jday = int(t_elapse_min/1440d0 - julday(year,1,1d0) + 1)
-         flag = FLAG_NORMAL
+      t_total_s  = t_ed_s - t_st_s
+      t_remain_s = t_total_s
+      t_elapse_s = 0d0
 
+      ntstep     = ceiling(t_total_s / timestep(IP_BASE))
+      itstep     = 0
+      year       = year_st
+      month      = month_st
+      day        = day_st
+      minute     = minute_st
+      second     = second_st
+      jday       = julday(year_st, month_st, frac_day_st) 
+     +           - julday(year_st, 1, 1d0) 
 
-
-      elseif (itstep.eq.ntstep) then
-         ts_model_min = t_total_min - t_elapse_min
-         ts_chem_min  = ts_model_min
-         t_remain_min = 0
-         t_elapse_min = t_total_min
-         jd = (t_st_min+t_elapse_min)/1440d0
-         call jd2ymdhms(jd,year,month,day,hour,minute,second)
-         jday = int(t_elapse_min/1440d0 - julday(year,1,1d0) + 1)
-         flag = FLAG_LAST
-      elseif (itstep.gt.ntstep) then 
-         print*,'Simulation finished'
-         flag = FLAG_END
-      endif
-      if (flag.ne.FLAG_END) print "(A,i4,'-',i2,'-',i2,x,i2,':',i2)",
-     +      'Current time:',year,month,day,hour,minute
-      endfunction time_step_next
+      ifdoit     = .true.
+      endsubroutine time_settime      
+!======================================================================      
 
       endmodule module_time_common
